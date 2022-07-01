ab84c19230d29d010cb5b8eb3affe52c9a38a994
Felddy <markf@geekpad.com>
2022-07-01 17:46:21 -0400

    Rework action parameters to allow files or strings

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import {generate} from './generate'
+import {validate} from './validate'
 import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const template_filename: string = core.getInput('template')
-    const input_object: string = core.getInput('json')
-    const output_filename: string = core.getInput('dest')
-    const result = await generate(
-      template_filename,
-      input_object,
-      output_filename
+    const input_file: string = core.getInput('input_file')
+    const input: string = core.getInput('input')
+    const output_file: string = core.getInput('output_file')
+    const template_file: string = core.getInput('template_file')
+    const template: string = core.getInput('template')
+
+    const [template_data, input_data]: [string, object] = await validate(
+      input_file,
+      input,
+      template_file,
+      template
+    )
+
+    const result: string = await generate(
+      template_data,
+      input_data,
+      output_file
     )
     core.setOutput('result', result)
   } catch (error) {
